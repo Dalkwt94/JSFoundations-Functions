b8ac0c8335f359b845b0c25c60989a2976405583
Dee Macbook <deemacbook@Dees-MacBook-Pro.local>
2020-06-16 20:29:10 +0300

    solved the task

--- functions.js
+++ functions.js
@@ -1,53 +1,40 @@
-/**
- * greet(name):
- * - receives a name
- * - logs "Hello <name>"
- *
- * e.g.
- * greet("Michael") logs "Hello Michael"
- */
 function greet(name) {
-  // Your code here
+  console.log(`Hello ${name}`);
 }
+greet("dalal");
 
-/**
- * isOdd(n):
- * - receives a number n
- * - returns true if it's odd, false otherwise
- *
- * e.g.
- * isOdd(7) -> true
- * isOdd(10) -> false
- */
+//===========================================================================//
+
 function isOdd(n) {
-  // Your code here
+  if (n % 2 !== 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
+isOdd(8);
 
-/**
- * oddsSmallerThan(n):
- * - receives a number n
- * - returns the number of ODD numbers smaller than n
- *
- * e.g.
- * oddsSmallerThan(7) -> 3
- * oddsSmallerThan(15) -> 7
- */
+//===========================================================================//
+
 function oddsSmallerThan(n) {
-  // Your code here
+  let s = n / 2;
+  return Math.floor(s);
 }
 
-/**
- * squareOrDouble(n):
- * - receives a number n
- * - returns its square if it's odd
- * - returns its double if it's even
- *
- * e.g.
- * squareOrDouble(16) -> 32
- * squareOrDouble(9) -> 81
- */
+//let res = oddsSmallerThan(7);
+//console.log(res);
+
+//===========================================================================//
+
 function squareOrDouble(n) {
-  // Your code here
+  if (n % 2 !== 0) {
+    return n * n;
+  } else if (n % 2 === 0) {
+    return n + n;
+  }
 }
+
+//let res2 = squareOrDouble(8);
+//console.log(res2);
 
 module.exports = { greet, isOdd, oddsSmallerThan, squareOrDouble };
